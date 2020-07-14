e2ce165d9c40e6cfd93f659495fff470c1952932
JackSnowdon <jacksnowdondrums@gmail.com>
2020-07-14 17:23:06 +0100

    added styling and accounts

--- distanced/settings.py
+++ distanced/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'accounts',
     'home',
 ]
 
@@ -120,3 +122,5 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
